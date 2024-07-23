1a46f56395bbb9889a56f461454938d17df5e0fd
hanlintao <pkucater@gmail.com>
2024-07-23 19:38:30 +0800

    Bug Fix

--- shared/head.php
+++ shared/head.php
@@ -15,7 +15,7 @@ include "resource.php";
 <link href="css/bootstrap.min.css" rel="stylesheet">
  
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-<script src="js/2.1.1/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
  
 <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 <script src="js/bootstrap.min.js"></script>
